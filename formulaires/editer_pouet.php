dc0369b0c75c22844f64fcb043e5cb9ac4f212b7
Cerdic <cedric@yterium.com>
2017-09-18 18:00:49 +0200

    renommage

--- formulaires/editer_pouet.php
+++ formulaires/editer_pouet.php
@@ -14,15 +14,15 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 /**
  * Fonction de chargement des valeurs par defaut des champs du formulaire
  */
-function formulaires_editer_microblog_charger_dist($objet,$id_objet,$hide_form=false){
+function formulaires_editer_pouet_charger_dist($objet,$id_objet,$hide_form=false){
 	$primary = id_table_objet($objet);
 
 	$valeurs = array();
-	$valeurs['_hide'] = (($hide_form AND is_null(_request('microblog')))?' ':'');
+	$valeurs['_hide'] = (($hide_form AND is_null(_request('pouet')))?' ':'');
 	$valeurs['objet']=$objet;
 	$valeurs['id_objet']=$id_objet;
-	$valeurs['microblog'] = recuperer_fond("modeles/microblog_instituer".$objet,array($primary=>$id_objet));
-	$valeurs['_status'] = trim($valeurs['microblog']);
+	$valeurs['pouet'] = recuperer_fond("modeles/mastodon_instituer".$objet,array($primary=>$id_objet));
+	$valeurs['_status'] = trim($valeurs['pouet']);
 
 	include_spip("inc/mastodon");
 	if (!mastodon_verifier_config()){
@@ -39,12 +39,12 @@ function formulaires_editer_microblog_charger_dist($objet,$id_objet,$hide_form=f
  * Vérifie la présence d'un statut depuis le champs adéquat
  * Vérifie que la longueur du statut n'excède pas la longueur maximale
  */
-function formulaires_editer_microblog_verifier_dist($objet,$id_objet){
+function formulaires_editer_pouet_verifier_dist($objet,$id_objet){
 	include_spip('inc/charsets');
 	$erreurs = array();
-	$microblog = _request('microblog');
-	if (spip_strlen($microblog)>140){
-		$erreurs['microblog'] = _T('mastodon:longueur_maxi_status');
+	$pouet = _request('pouet');
+	if (spip_strlen($pouet)>500){
+		$erreurs['pouet'] = _T('mastodon:longueur_maxi_status');
 	}
 
 	return
@@ -58,13 +58,13 @@ function formulaires_editer_microblog_verifier_dist($objet,$id_objet){
  * S'il y a une erreur en retour (false), 
  * on affiche un message explicitant qu'il y a une erreur dans la configuration
  */
-function formulaires_editer_microblog_traiter_dist($objet,$id_objet){
+function formulaires_editer_pouet_traiter_dist($objet,$id_objet){
 	$res = array('editable'=>true);
-	$microblog = _request('microblog');
-	if (_request('annuler_microblog'))
-		$microblog = " ";// ruse pour ne rien envoyer
-	if (!is_null($microblog)){
-		$set = array('microblog'=>$microblog);
+	$pouet = _request('pouet');
+	if (_request('annuler_pouet'))
+		$pouet = " ";// ruse pour ne rien envoyer
+	if (!is_null($pouet)){
+		$set = array('pouet'=>$pouet);
 		if (include_spip('action/editer_objet')
 		  AND function_exists('objet_modifier'))
 			objet_modifier($objet, $id_objet, $set);
@@ -72,14 +72,14 @@ function formulaires_editer_microblog_traiter_dist($objet,$id_objet){
 		  AND function_exists($f="revision_$objet"))
 			$f($id_objet, $set);
 	}
-	if (!strlen(trim($microblog)))
-		set_request('microblog');
+	if (!strlen(trim($pouet)))
+		set_request('pouet');
 
 	if (_request('envoyer')){
-		include_spip('inc/microblog');
+		include_spip('inc/pouet');
 		$primary = id_table_objet($objet);
-		$status = recuperer_fond("modeles/microblog_instituer".$objet,array($primary=>$id_objet));
-		$retour = microblog($status);
+		$status = recuperer_fond("modeles/mastodon_instituer".$objet,array($primary=>$id_objet));
+		$retour = pouet($status);
 		if($retour){
 			$res['message_ok']=_T('mastodon:message_envoye')." ".$status;
 		}
