ef0f22a3043eb1df8e4b7b2b3f8eda5bf80b3143
Cerdic <cedric@yterium.com>
2017-09-19 11:18:07 +0200

    branchement de la boucle DATA et update du modele qui sert a afficher une timeline

--- inc/mastodon_to_array.php
+++ inc/mastodon_to_array.php
@@ -10,7 +10,12 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-
+/**
+ * Boucle (DATA){
+ * @param $url
+ * @param array $options
+ * @return array|bool|string
+ */
 function inc_mastodon_to_array_dist($url, $options = array()) {
 
 	$url = parse_url($url);
@@ -18,6 +23,11 @@ function inc_mastodon_to_array_dist($url, $options = array()) {
 	$command = $url['path'];
 	$params = array();
 	parse_str($url['query'],$params);
+
+	if (isset($params['mastodon_account'])) {
+		$options['mastodon_account'] = $params['mastodon_account'];
+		unset($params['mastodon_account']);
+	}
 
 	if (!function_exists('mastodon_api_call'))
 		include_spip("inc/mastodon");
